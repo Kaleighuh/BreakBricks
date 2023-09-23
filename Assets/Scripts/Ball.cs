ba48404a80c149855b9ac8867e97569513d51c84
Kaleighuh <10958501@uvu.edu>
2023-09-22 23:54:01 -0600

    Wrapping up for the night

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,6 +4,7 @@ public class Ball : MonoBehaviour
 {
     public new Rigidbody2D rigidbody { get; private set; }
     public float speed = 500f;
+    public float maxBounceAngle = 75f;
     
     private void Awake()
     {
@@ -22,5 +23,25 @@ public class Ball : MonoBehaviour
         force.y = -1f;
 
         this.rigidbody.AddForce(force.normalized * this.speed);
+    }
+
+    private void OnCollistionEnter2D(Collision2D collision)
+    {
+        Ball ball = collision.gameObject.GetComponent<Ball>();
+        if (ball != null)
+        {
+            Vector3 paddlePosition = this.transform.position;
+            Vector2 contactPoint = collision.GetContact(0).point;
+
+            float offset = paddlePosition.x - contactPoint.x;
+            float width = collision.otherCollider.bounds.size.x / 2;
+
+            float currentAngle = Vector2.SignedAngle(Vector2.up, ball.rigidbody.velocity);
+            float bounceAngle = (offset / width) * this.maxBounceAngle;
+            float newAngle = Mathf.Clamp(currentAngle + bounceAngle, -this.maxBounceAngle, this.maxBounceAngle);
+
+            Quaternion rotation = Quaternion.AngleAxis(newAngle, Vector3.forward);
+            ball.rigidbody.velocity = rotation * Vector2.up * ball.rigidbody.velocity.magnitude;
+        }
     }
 }
